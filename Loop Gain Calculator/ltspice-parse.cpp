199b1267b483d8536dcc41e66f13300d58f33a68
aaronlockhartdev <aaronlockhartdev@gmail.com>
2023-11-26 03:15:28 -0500

    Began implementing LtSPICE raw file parsing

--- Loop Gain Calculator/ltspice-parse.cpp
+++ Loop Gain Calculator/ltspice-parse.cpp
@@ -1,9 +1,36 @@
+#include <filesystem>
 #include <fstream>
+#include <sstream>
+#include <regex>
 
 #include "ltspice-parse.hpp"
 
-ltResponse parse(string path) {
-    ifstream file;
-    file.open(path, ios::in | ios::binary);
-    
+ltResponse *parse(filesystem::path path)
+{
+    wifstream file(path, ios::in | ios::binary);
+
+    u16string endHeader = u"Binary:\n"s;
+
+    vector<wchar_t> headerVec;
+
+    while (true)
+    {
+        wchar_t c = file.get();
+
+        if (c == EOF)
+            return NULL;
+
+        headerVec.push_back(c);
+
+        if (
+            headerVec.size() >= endHeader.length() &&
+            u16string(headerVec.end() - endHeader.length(), headerVec.end()) == endHeader)
+            break;
+    }
+
+    u16string header(headerVec.begin(), headerVec.end());
+
+    string regex =
+        ""
+        "";
 }
