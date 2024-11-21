9f50a6e8946ee8c50e4a789c42ce3b8e91a859b8
Aaron Lockhart <aaronlockhartdev@gmail.com>
2024-11-21 15:24:52 -0500

    Fixed jupyter.sh

--- loop_analysis/python/jupyter.sh
+++ loop_analysis/python/jupyter.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 
 # Add venv to jupyter
-<<<<<<< Updated upstream
 . ./.venv/bin/activate
-=======
-. .venv/bin/activate
->>>>>>> Stashed changes
 pip install pip ipykernel -U
 python -m ipykernel install --user --name=class_d_amp
 deactivate
 
-<<<<<<< Updated upstream
 # Use symengine backend
-=======
-# Use SYMENGINE backend for SymPy
->>>>>>> Stashed changes
 export USE_SYMENGINE=1
 
 # Start jupyter
