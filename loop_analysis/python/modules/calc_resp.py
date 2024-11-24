Fixed bug with calculation

loop_analysis/python/modules/calc_resp.py
@@ -38,7 +38,7 @@ def calc_resp(
     dcgains = np.empty((n_samples, hs.size))
 
     for i in prange(n_samples):
-        fr_tf = npp.polyval(fxn, -num_coefs[:,i]) / npp.polyval(fxn, den_coefs[:,i]) * np.exp(-delays[i] * fxn)
+        fr_tf = npp.polyval(fxn, -num_coefs[i]) / npp.polyval(fxn, den_coefs[i]) * np.exp(-delays[i] * fxn)
 
         fresp = np.zeros((hs.size, fs.size), dtype=np.complex128)
         for j in range(hs.size):
@@ -66,7 +66,7 @@ def calc_resp(
                     break
                 mid = (start + end) / 2
                 xn = ns * mid
-                tmp[j] = npp.polyval(xn, -num_coefs[:,i]) / npp.polyval(xn, den_coefs[:,i]) * np.exp(-delays[i] * xn)
+                tmp[j] = npp.polyval(xn, -num_coefs[i]) / npp.polyval(xn, den_coefs[i]) * np.exp(-delays[i] * xn)
                 err = np.angle(np.sum(tmp[j] * fr_tmp[j,:]))
 
                 if np.signbit(err): end = mid

loop_analysis/python/modules/problem.py
@@ -32,7 +32,7 @@ class LoopOptimization(Problem):
 
         super().__init__(
             n_var=len(self._vars) + 1,
-            n_obj=2,
+            n_obj=3,
             xl=xl,
             xu=xu,
             vtype=float
@@ -48,16 +48,18 @@ class LoopOptimization(Problem):
             delays,
             *self._eval_consts
         )
-
+        
         fs, hs, ns = self._eval_consts[:3]
 
         obj_lst = list()
-        obj_lst.append(abs(osc_frs[:,-1] / (2 * np.pi) - 500e3))
+        obj_lst.append(np.nan_to_num(abs(osc_frs[:,-1] / (2 * np.pi) - 500e3)))
         obj_lst.append(np.nan_to_num(1.01 ** -np.min((mag[:,-1] * dcgains[:,-1,None])[:,np.imag(fs) < 2 * np.pi * 20e3], axis=-1)))
+        p_band_ks = dcgains[:,hs > 0.25]
+        pb_max = np.max(p_band_ks, axis=-1)
+        pb_min = np.min(p_band_ks, axis=-1)
+        obj_lst.append(np.nan_to_num((pb_max - pb_min) / pb_max))
 
-        print(osc_frs.shape)
-        print(osc_frs[:,-1] / (2 * np.pi))
-
+        
         out['F'] = obj_lst
 
     @classmethod

loop_analysis/python/modules/sapwin.py
@@ -90,8 +90,6 @@ class Sapwin(LoopOptimization):
             def to_func(exprs: List[str]):
                 sym_exprs = list(map(se.S, exprs))
 
-                print(exprs)
-
                 lmbda = se.Lambdify(
                     [syms], 
                     sym_exprs, 
@@ -99,13 +97,14 @@ class Sapwin(LoopOptimization):
                     backend='llvm'
                 )
 
-                num_coefs = len(exprs)
+                n_coefs = len(exprs)
 
                 #@njit(nogil=True, fastmath=True, parallel=True)
                 def f(vals):
-                    res = np.empty((vals.shape[0], num_coefs))
-                    for i in prange(vals.shape[0]):
-                        res[i] = np.array(lmbda(vals[i]), dtype=float).T
+                    n_samples = vals.shape[0]
+                    res = np.empty((n_samples, n_coefs))
+                    for i in prange(n_samples):
+                        res[i] = lmbda(vals[i])
                     return res
 
                 return f